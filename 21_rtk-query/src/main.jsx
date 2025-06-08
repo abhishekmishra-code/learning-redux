5a5c8156a11e54620e16ab75e0baaf8cdc50dd2b
Abhishek Mishra <abhishekmishra0101010@gmail.com>
2025-06-08 14:28:40 +0530

    rtk-query

---
+++ 21_rtk-query/src/main.jsx
@@ -0,0 +1,35 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import { Provider } from 'react-redux'
+import { store } from './store.js'
+import Home from './Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    ),
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/contact',
+        element: <p>Contact Us</p>,
+      },
+    ],
+  },
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
