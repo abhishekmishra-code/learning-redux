7f021d0061596f54d90432197fc2e871246abdf2
Abhishek Mishra <abhishekmishra0101010@gmail.com>
2025-06-08 14:24:20 +0530

    middleware-in-redux

---
+++ 15_middleware-in-redux/src/store/slices/cartSlice.js
@@ -0,0 +1,42 @@
+import { myCreateSlice } from '../../../redux-toolkit'
+
+const findIndexItem = (state, action) =>
+  state.findIndex((cartItem) => cartItem.productId === action.payload.productId)
+
+const mySlice = myCreateSlice({
+  name: 'cart',
+  initialState: [],
+  reducers: {
+    addCartItem(state, action) {
+      const existingItemIndex = findIndexItem(state, action)
+      if (existingItemIndex !== -1) state[existingItemIndex].quantity += 1
+      else state.push({ ...action.payload, quantity: 1 })
+    },
+
+    removeCartItem(state, action) {
+      const existingItemIndex = findIndexItem(state, action)
+      state.splice(existingItemIndex, 1)
+    },
+
+    increaseCartItemQuantity(state, action) {
+      const existingItemIndex = findIndexItem(state, action)
+      state[existingItemIndex].quantity += 1
+    },
+
+    decreaseCartItemQuantity(state, action) {
+      const existingItemIndex = findIndexItem(state, action)
+      state[existingItemIndex].quantity -= 1
+      if (state[existingItemIndex].quantity < 1)
+        state.splice(existingItemIndex, 1)
+    },
+  },
+})
+
+export const {
+  addCartItem,
+  removeCartItem,
+  increaseCartItemQuantity,
+  decreaseCartItemQuantity,
+} = mySlice.actions
+
+export default mySlice.reducer
